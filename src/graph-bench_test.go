e0834e4054bff3734a1ac25f16606c3365ee12b7
Emmanuel Odeke <odeke@ualberta.ca>
2015-11-11 02:38:46 -0700

    benchmarks update for bfs+dfs

--- src/graph-bench_test.go
+++ src/graph-bench_test.go
@@ -25,3 +25,57 @@ func BenchmarkMultiVertexInsertion(b *testing.B) {
 		}
 	}
 }
+
+func dfsRun() {
+	g := NewGraph()
+
+	nodes := map[int][]int{
+		2:  []int{0, 2, 9, 10, 12},
+		8:  []int{9, 3, 1},
+		1:  []int{},
+		11: []int{12, 19, 8},
+        -1: []int{2, 8, 1, 11, -1, 3, 9, 12, -1000},
+	}
+
+	for src, nbs := range nodes {
+		for _, nb := range nbs {
+			g.UpdateEdge(src, nb, -1)
+		}
+	}
+
+	DFSOnFresh(g)
+}
+
+func bfsRun() {
+	g := NewGraph()
+
+	nodes := map[int][]int{
+		2:  []int{0, 2, 9, 10, 12},
+		8:  []int{9, 3, 1},
+		1:  []int{},
+		11: []int{12, 19, 8},
+        -1: []int{2, 8, 1, 11, -1, 3, 9, 12, -1000},
+	}
+
+	for src, nbs := range nodes {
+		for _, nb := range nbs {
+			g.UpdateEdge(src, nb, -1)
+		}
+	}
+
+    for src, _ := range nodes {
+	    BFSOnFresh(g, src)
+    }
+}
+
+func BenchmarkDFS(b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        dfsRun()
+    }
+}
+
+func BenchmarkBFS(b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        bfsRun()
+    }
+}
